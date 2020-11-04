b0484bf036b5747520064f880bb8f03c94b03fac
souleymane kone <desolo74@gmail.com>
2020-11-03 23:20:12 -0800

    process

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-`use strict`
+`use strict`;
+
+function getUrlList(username){
+    fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(reponseJson =>
+     console.log(reponseJson))
+   
+}
+/**function getUrlList(username){
+    fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(reponseJson =>
+     console.log(reponseJson))
+   
+}
+ */
+
+function watchForm(){
+$('form').submit(event =>{
+event.preventDefault();
+console.log('watchForm section');
+let numberDogs = $('.enter').val();
+     $('.enter').val("");
+     console.log(watchForm(numberDogs));
+
+})
+
+}
+$(function(){
+    console.log('app loaded! Waiting for submit');
+    watchForm();
+
+});
+
+
+
+
+
+
