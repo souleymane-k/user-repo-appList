c64c8b53e3f8ead0ceed1406fb402b8b35850a46
souleymane kone <desolo74@gmail.com>
2020-11-04 17:05:29 -0800

    finalChanged

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-`use strict`;
+`use strict`
 
-function getUrlList(username){
-    fetch(`https://api.github.com/users/${username}/repos`)
-    .then(response => response.json())
-    .then(reponseJson =>
-     console.log(reponseJson))
-   
-}
-/**function getUrlList(username){
-    fetch(`https://api.github.com/users/${username}/repos`)
-    .then(response => response.json())
-    .then(reponseJson =>
-     console.log(reponseJson))
-   
-}
- */
+function getUserInfo(inputName) {
+    fetch("https://api.github.com/users/"+ inputName)
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'));     
+    
+  }
+  
+  function displayResults(responseJson) {
+    console.log(responseJson);
+    $('.results').empty()
+    //replace the existing image with the new one
+    $('.results').replaceWith( `
+    <ul>
+    <div>${responseJson.name}</div>
+    <div>${responseJson.html_url}</div>
+    </ul>
+    `
+    )
+  
+    //display the results section
+    $('.results').removeClass('hidden');
+  
+  }
+
+/********************************** */
 
 function watchForm(){
 $('form').submit(event =>{
 event.preventDefault();
 console.log('watchForm section');
-let numberDogs = $('.enter').val();
-     $('.enter').val("");
-     console.log(watchForm(numberDogs));
+let userInput = $('.enter').val();
+     $('.enter').val(" ");
+     let inputName = userInput.split(' ').join('')
+     getUserInfo(inputName);    
 
 })
 
 }
+
 $(function(){
     console.log('app loaded! Waiting for submit');
     watchForm();
